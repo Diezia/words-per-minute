92a8f16ad6b62765c766890a4c9c4e697c5e0d77
diezia <diezyael@gmail.com>
2021-12-21 19:23:25 -0300

    webvitals bug fix

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { MyApp } from './MyApp';
